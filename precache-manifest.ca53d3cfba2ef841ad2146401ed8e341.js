9346bcc9072f829d02e29c566a1eccfd33b6ea8e
Michael Cumings <michael.cumings@gmail.com>
2018-12-04 08:38:10 -0600

    Updates

---
+++ precache-manifest.ca53d3cfba2ef841ad2146401ed8e341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "5a107b1afe2cc39a39aa",
+    "url": "/clickygame/static/js/main.5a107b1a.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/clickygame/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "5a107b1afe2cc39a39aa",
+    "url": "/clickygame/static/css/main.3a0b81c4.chunk.css"
+  },
+  {
+    "revision": "c0b1d085c028c601a6ac981a99135b61",
+    "url": "/clickygame/index.html"
+  }
+];
